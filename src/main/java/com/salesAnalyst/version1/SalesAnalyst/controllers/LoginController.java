e992766c9c3fb4fb40d2532a091801ee48f4c235
PravindaPerera <pravinda.joes@gmail.com>
2018-05-13 18:58:07 +0530

    current month and year displayed | chart values provided from backend

--- src/main/java/com/salesAnalyst/version1/SalesAnalyst/controllers/LoginController.java
+++ src/main/java/com/salesAnalyst/version1/SalesAnalyst/controllers/LoginController.java
@@ -3,9 +3,14 @@ package com.salesAnalyst.version1.SalesAnalyst.controllers;
 import com.salesAnalyst.version1.SalesAnalyst.entities.Users;
 import com.salesAnalyst.version1.SalesAnalyst.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Calendar;
 
 @Controller
 @SessionAttributes("username")
@@ -13,8 +18,13 @@ public class LoginController {
     @Autowired // This means to inject the bean called userRepository
     private UsersRepository userRepository;
 
+    @Value("${app.timezone}")
+    private String timezone;
+    @Value("${app.months}")
+    private String months;
+
     @PostMapping(path="/login") // Map ONLY POST Requests with the path /login
-    public String handleUserLogin(ModelMap model, @RequestParam String username,
+    public String handleUserLogin(RedirectAttributes redirectAttributes, @RequestParam String username,
                                   @RequestParam String password) {
 
         Users user  = userRepository.findByUsernameAndPassword(username, password);
@@ -22,7 +32,7 @@ public class LoginController {
         if (user == null) { // If user entered incorrect credentials
             return "redirect:/login/error";
         } else { // successful login
-            model.put("username", username);
+            redirectAttributes.addFlashAttribute("user", user);
             return "redirect:/user/dashboard";
         }
     }
@@ -40,7 +50,22 @@ public class LoginController {
     }
 
     @GetMapping(path="/user/dashboard") // Map ONLY GET Requests with the path /user/dashboard
-    public String handleUponSuccessUserLogin() {
+    public String handleUponSuccessUserLogin(ModelMap model, @ModelAttribute("user") Users currentUser) {
+        String[] monthValues = months.split(",");
+
+        // send username to be displayed in the dashboard
+        model.addAttribute("username", currentUser.getUsername());
+
+        // current year and current month for users timezone needs to be obtained
+        java.util.TimeZone tz = java.util.TimeZone.getTimeZone(timezone);
+        java.util.Calendar c = java.util.Calendar.getInstance(tz);
+
+        model.addAttribute("currentYear", c.get(Calendar.YEAR));
+        model.addAttribute("currentMonth", monthValues[c.get(Calendar.MONTH)]);
+
+        // current months sales/cost vales need to be displayed
+        model.addAttribute("salesValue", 100);
+        model.addAttribute("costValue", 30);
         return "dashboard";
     }
 
