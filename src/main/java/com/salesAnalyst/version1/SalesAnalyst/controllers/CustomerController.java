e9e7754b756dad81c31f71d855ae5987714bf2e3
PravindaPerera <pravinda.joes@gmail.com>
2018-12-23 19:00:09 +0530

    customer and product page modifications

--- src/main/java/com/salesAnalyst/version1/SalesAnalyst/controllers/CustomerController.java
+++ src/main/java/com/salesAnalyst/version1/SalesAnalyst/controllers/CustomerController.java
@@ -15,8 +15,13 @@ public class CustomerController {
     @Value("${app.timezone}")
     private String timezone;
 
+    @Value("${app.months}")
+    private String months;
+
     @GetMapping(path="/user/customer") // Map ONLY GET Requests with the path /user/customer
     public String handleCustomerViewDisplay(ModelMap model) {
+
+        String[] monthValues = months.split(",");
 
         if (!model.containsAttribute("username")) {
             return "redirect:/";
@@ -33,11 +38,34 @@ public class CustomerController {
         model.addAttribute("customerBusiness", "Restaurant Management");
         model.addAttribute("customerAddress", "555A, Canal Row, Colombo 01");
 
+        // array of company total sales each month
+        int[] totalMonthlySales = new int[] {500, 400, 325, 190, 180, 800, 600, 500, 630, 470, 0, 0};
         // array of monthly sales of the customer
-        int[] monthlySales = new int[] {100, 200, 125, 70, 80, 300, 200, 100, 130, 270, 0, 0};
+        int[] customerMonthlySales = new int[] {100, 200, 125, 70, 80, 300, 200, 100, 130, 270, 0, 0};
+        // array containing monthly customer sales proportionality to total monthly sales
+        double[] salesPropotion = new double[12];
+        for (int i=0; i<totalMonthlySales.length; i++) {
+            if (totalMonthlySales[i] == 0) {
+                salesPropotion[i] = 0;
+            } else {
+                salesPropotion[i] = ((double) customerMonthlySales[i]/totalMonthlySales[i]) * 100;
+            }
+        }
+        // array of product sales for the focused customer
         int[] customerProductSales = new int[] {600, 180, 250, 300, 170, 190};
-        model.addAttribute("monthlySales", monthlySales);
+        // product names of the company
+        String[] productNames = new String[] {"Product1", "Product2", "Product3", "Product4", "Product5", "Product6"};
+        // colour codes for each product
+        String[] colourCodes = new String[] {"#26B99A", "#26B9B9", "#268AB9", "#264CB9", "#5326B9", "#8A26B9"};
+
+        model.addAttribute("totalMonthlySales", totalMonthlySales);
+        model.addAttribute("customerMonthlySales", customerMonthlySales);
         model.addAttribute("customerProductSales", customerProductSales);
+        model.addAttribute("productNames", productNames);
+        model.addAttribute("colourCodes", colourCodes);
+        model.addAttribute("monthValues", monthValues);
+        model.addAttribute("salesPropotion", salesPropotion);
+        model.addAttribute("notApplicable", "N/A");
         return "customer";
     }
 }
--- src/main/java/com/salesAnalyst/version1/SalesAnalyst/controllers/ProductController.java
+++ src/main/java/com/salesAnalyst/version1/SalesAnalyst/controllers/ProductController.java
@@ -15,8 +15,13 @@ public class ProductController {
     @Value("${app.timezone}")
     private String timezone;
 
+    @Value("${app.months}")
+    private String months;
+
     @GetMapping(path="/user/product") // Map ONLY GET Requests with the path /user/product
     public String handleCustomerViewDisplay(ModelMap model) {
+
+        String[] monthValues = months.split(",");
 
         if (!model.containsAttribute("username")) {
             return "redirect:/";
@@ -28,14 +33,27 @@ public class ProductController {
 
         model.addAttribute("currentYear", c.get(Calendar.YEAR));
 
-        // customer details
+        // product details
         model.addAttribute("productName", "Product A");
 
-        // array of monthly sales of the customer
-        int[] monthlySales = new int[] {100, 200, 125, 70, 80, 300, 200, 100, 130, 270, 0, 0};
-        int[] customerProductSales = new int[] {600, 180, 250, 300, 170, 190};
-        model.addAttribute("monthlySales", monthlySales);
-        model.addAttribute("customerProductSales", customerProductSales);
+        // array of company total sales each month
+        int[] totalMonthlySales = new int[] {500, 400, 325, 190, 180, 800, 600, 500, 630, 470, 0, 0};
+        // array of monthly sales of the focused product
+        int[] monthlyProductSales = new int[] {100, 200, 125, 70, 80, 300, 200, 100, 130, 270, 0, 0};
+        // array containing monthly product sales proportionality to total monthly sales
+        double[] salesPropotion = new double[12];
+        for (int i=0; i<totalMonthlySales.length; i++) {
+            if (totalMonthlySales[i] == 0) {
+                salesPropotion[i] = 0;
+            } else {
+                salesPropotion[i] = ((double) monthlyProductSales[i]/totalMonthlySales[i]) * 100;
+            }
+        }
+        model.addAttribute("totalMonthlySales", totalMonthlySales);
+        model.addAttribute("monthlyProductSales", monthlyProductSales);
+        model.addAttribute("salesPropotion", salesPropotion);
+        model.addAttribute("monthValues", monthValues);
+        model.addAttribute("notApplicable", "N/A");
         return "product";
     }
 }
